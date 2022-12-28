061c790dceadd1c74982afc2e09a5d07f355fec6
TuDang1 <dangquoctuhn@gmail.com>
2022-12-28 15:48:50 +0700

    Init server

---
+++ server/README.md
@@ -0,0 +1,47 @@
+# Travelo Server
+
+
+
+
+## Installation
+
+Install mongodb compass 
+```console
+https://www.mongodb.com/try/download/shell
+```
+
+
+Install library:
+
+```console
+$ npm install
+```
+
+Init database:
+
+```console
+$ npm run seed
+```
+
+## Document API
+
+
+## Start Server
+
+```console
+$ npm start
+```
+
+Start the server (dev):
+
+```console
+$ npm run dev
+```
+
+
+
+Test server
+
+```console
+http://localhost:5000/api/location/list-provinces
+```
---
+++ server/app.js
@@ -0,0 +1,41 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const logger = require('morgan');
+const i18next = require('i18next');
+const Backend = require('i18next-fs-backend');
+const middleware = require('i18next-http-middleware');
+
+
+i18next
+  .use(Backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    backend: {
+      loadPath: './locales/{{lng}}/translation.json',
+    },
+  });
+
+const app = express();
+app.use(middleware.handle(i18next));
+
+// enabling cors for all requests by using cors middleware
+const corsOptions = {
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'],
+};
+app.use(cors(corsOptions));
+// Enable pre-flight
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+module.exports = app;
