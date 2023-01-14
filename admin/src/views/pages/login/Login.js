8a78580c48d434072124b14c660efe8475fcaf34
Bui Quoc Thuan <thuanhehehehe@gmail.com>
2023-01-14 10:10:18 +0700

    login and profile page

--- admin/src/views/pages/login/Login.js
+++ admin/src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import {
   CButton,
   CCard,
@@ -14,72 +14,114 @@ import {
   CRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import { storeUserData } from 'src/services/auth'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from 'src/redux/actions/user'
+import Modal from '../../../components/modal/Modal'
+import { Form, Input, Button, Checkbox, message, Radio } from 'antd'
+import { PhoneOutlined, LockOutlined } from '@ant-design/icons'
+import { Roles } from '../../../config/Roles'
 
+var _ = require('lodash')
 const Login = () => {
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user)
+  const navigate = useNavigate()
+
+  const onFinish = (values) => {
+    const data = {}
+    data.email = values.email
+    data.password = values.password
+    data.role = values.role
+    const loginThunk = login(data)
+    dispatch(loginThunk)
+  }
+  useEffect(() => {
+    if (!_.isEmpty(user.data)) {
+      storeUserData(user.data)
+      navigate('/')
+    }
+  }, [user])
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
-                          Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
+    <>
+      <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+        <CContainer>
+          <CRow className="justify-content-center">
+            <CCol md={5}>
+              <CCardGroup>
+                <CCard className="p-4">
+                  <CCardBody>
+                    <h2 style={{ marginBottom: '10px' }}>Travelo ADMIN</h2>
+                    <Form name="normal_login" className="login-form" onFinish={onFinish}>
+                      <Form.Item
+                        name="email"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please input your email!',
+                          },
+                        ]}
+                      >
+                        <Input
+                          prefix={<PhoneOutlined className="site-form-item-icon" />}
+                          placeholder={'Email'}
+                        />
+                      </Form.Item>
+                      <Form.Item
+                        name="password"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please input your Password!',
+                          },
+                        ]}
+                      >
+                        <Input
+                          prefix={<LockOutlined className="site-form-item-icon" />}
+                          type="password"
+                          placeholder={'Password'}
+                        />
+                      </Form.Item>
+                      <Form.Item
+                        name="role"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please select role',
+                          },
+                        ]}
+                      >
+                        <Radio.Group>
+                          <Radio value={Roles.ADMIN}>{'Admin'}</Radio>
+                          <Radio value={Roles.SALE}>{'Sale'}</Radio>
+                          <Radio value={Roles.EDITOR}>{'Editor'}</Radio>
+                        </Radio.Group>
+                      </Form.Item>
+                      <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                          <Checkbox>{'Remember me'}</Checkbox>
+                        </Form.Item>
+
+                        {/*<Link className="login-form-forgot float-right" to="">
+                          {'Forgot password'}
+                      </Link>*/}
+                      </Form.Item>
+
+                      <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                          {'Login'}
+                        </Button>
+                      </Form.Item>
+                    </Form>
+                  </CCardBody>
+                </CCard>
+              </CCardGroup>
+            </CCol>
+          </CRow>
+        </CContainer>
+      </div>
+    </>
   )
 }
 
