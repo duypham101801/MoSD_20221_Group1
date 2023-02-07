ec20475357ab5c7e42120a94c40bfb818097631d
vietanh3769 <79761099+vietanh3769@users.noreply.github.com>
2023-02-07 17:39:10 +0700

    update house controller

--- server/controllers/houseController.js
+++ server/controllers/houseController.js
@@ -36,3 +36,61 @@ exports.getListHouses = async (req, res, next) => {
       return next(new ServerException(error));
     }
   };
+
+// [POST] Add new house
+exports.postAddHouse = async (req, res, next) => {
+  try {
+    const pathAvatar = req.file.path.replace(/\\/g, '/');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      fs.unlinkSync(pathAvatar);
+      return next(new ValidationException(errors.array()));
+    }
+    const { houseName, houseType, housePrice, houseInfo, houseCount } =
+      req.body;
+
+    const houseCreated = await House.create({
+      areaId: req.params.areaId,
+      name: houseName,
+      urlAvatar: `${process.env.API_URL}${pathAvatar.replace(/public/, '')}`,
+      type: houseType,
+      price: housePrice,
+      info: houseInfo,
+      count: parseInt(houseCount, 10),
+    });
+
+    res.status(201).json({
+      message: req.t('create_success'),
+      house: houseCreated,
+    });
+  } catch (error) {
+    return next(new ServerException(error));
+  }
+};
+
+// [PUT] Update house detail
+exports.putHouseDetail = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(new ValidationException(errors.array()));
+    }
+
+    const { houseName, houseType, housePrice, houseInfo, houseCount } =
+      req.body;
+
+    await House.findByIdAndUpdate(req.params.houseId, {
+      name: houseName,
+      type: houseType,
+      price: housePrice,
+      info: houseInfo,
+      count: houseCount,
+    });
+
+    res.status(200).json({
+      message: req.t('update_success'),
+    });
+  } catch (error) {
+    return next(new ServerException(error));
+  }
+};
